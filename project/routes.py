7d442523a123c9eba1353c884c033cef189d1d6d
ScientistCoco <30433825+ScientistCoco@users.noreply.github.com>
2017-08-24 15:52:34 +1000

    Created routes to login pages and student page

--- project/routes.py
+++ project/routes.py
@@ -1,6 +1,15 @@
-from flask import Flask, redirect, render_template, request, url_for
+# Contains the redirects
+from flask import Flask, render_template, redirect, request, url_for
 from server import app
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+	return render_template("home_page.html")
+
+@app.route("/admin_login")
+def admin_login():
+	return render_template("login_page.html")
+
+@app.route("/student_page")
+def student_page():
+	return render_template("student_page.html")
