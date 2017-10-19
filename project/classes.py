e4441673128903cd663ae53a5f2de65c1cc878da
ScientistCoco <courtney.lum@gmail.com>
2017-10-19 23:24:51 +1100

    Fixed up the class, allowed students and staff to view survey results

--- project/classes.py
+++ project/classes.py
@@ -3,118 +3,16 @@ import os, json, csv, random, sqlite3
 from mvc import Controller, SurveyModel, SurveyView
 controller = Controller()
 
-class StudentDatabase():
+class Survey_system():
+	def __init__(self, course_filename, user = None):
+		self.user = user
+		self.course_filename = course_filename
 
-	def add_info_enrollments(self):
-		con = sqlite3.connect("survey_database.db")
-		cur = con.cursor()
-		cur.execute("CREATE TABLE IF NOT EXISTS student_enrolments (id int not null, course_name text not null, semester text not null, survey_completed text not null, num int primary key not null);") # use your column names here
-
-		with open('enrolments.csv','r') as f:
-     # csv.DictReader uses first line in file for column headings by default
-			dr = csv.DictReader(f)
-			increment = 0;
-			for i in dr:
-				increment = increment + 1
-				to_db = [(i['id'], i['course_name'], i['semester'],'no', increment)]
-				cur.executemany("INSERT OR REPLACE INTO student_enrolments (id, course_name, semester, survey_completed, num) VALUES (?, ?, ?, ?, ?);", to_db)
-				con.commit()
-		con.close()
-
-	def get_student_courses(self, id):
-		con = sqlite3.connect("survey_database.db")
-		cur = con.cursor()
-		rows = cur.execute("SELECT course_name FROM student_enrolments WHERE id = ?", (id,))
-		student_courses = []
-		for row in rows:
-			student_courses = student_courses + list(row)
-		con.close()
-		return student_courses
-
-	def get_student_semester(self, id):
-		con = sqlite3.connect("survey_database.db")
-		cur = con.cursor()
-		cur.execute("SELECT semester FROM student_enrolments WHERE id = ?", (id,))
-		semester = cur.fetchone()
-		con.close()
-		return semester[0]
-
-	def completion_of_survey(self, id, course_name):
-		con = sqlite3.connect("survey_database.db")
-		cur = con.cursor()
-		cur.execute("UPDATE student_enrolments SET survey_completed = 'yes' WHERE id = ? AND course_name = ?", (id, course_name))
-		con.commit()
-		con.close()
-
-	def check_if_survey_completed(self, id, course_name):
-		con = sqlite3.connect("survey_database.db")
-		cur = con.cursor()
-
-		cur.execute("SELECT survey_completed FROM student_enrolments WHERE id = ? AND course_name = ?", (id, course_name))
-		result = cur.fetchone()
-		con.close()
-		return result[0]
-
-class SurveyAvailability():
-	# Create the survey_availability table if it doesn't exist
-	def add_info(self):
-		con = sqlite3.connect("survey_database.db")
-		cur = con.cursor()
-		cur.execute("CREATE TABLE IF NOT EXISTS survey_availability (course_name text primary key not null, availability text);")
-
-		with open('courses.csv','r') as f:
-     # csv.DictReader uses first line in file for column headings by default
-			reader = csv.reader(f)
-			for field in reader:
-				text = field[0] + ' ' + field[1]
-				cur.execute("INSERT OR REPLACE INTO survey_availability (course_name) VALUES ('%s');" %(text))
-				con.commit()
-		con.close()
-
-class Admin:
-	def __init__(self):
-		pass
-
-	def open_questionfile(self):
-		questions = controller.view_all_questions()
+	def get_question(self, course_name):
+		# Returns the questions associated with the course
+		questions = []
+		questions = controller.search_surveyID(course_name)
 		return questions
-
-	def add_question(self, question_to_add, answer_to_add, questionType):
-		questionID = random.randint(1,50)
-		answerID = questionID
-		controller.add_questionText(questionID, question_to_add, questionType)
-		for answer in answer_to_add:
-			controller.add_answerText(answerID, questionID, answer)
-
-	def view_answers(self, question_to_see):
-		question_id = controller.search_questionID(question_to_see)
-		answer_list = controller.search_answerText(question_id)
-		return answer_list
-
-	def delete_question(self, question_to_delete):
-		question_id = controller.search_questionID(question_to_delete)
-		controller.delete_question(question_id)
-
-	def get_survey_availabilities(self):
-		survey_open = controller.count_survey_status('open')
-		survey_close = controller.count_survey_status('close')
-		survey_null = controller.count_survey_status('null')
-		availability = [survey_open, survey_close, survey_null]
-		return availability
-
-	def surveys_to_review(self):
-		con = sqlite3.connect("survey_database.db")
-		cur = con.cursor()
-		rows = cur.execute("SELECT course_name FROM survey_availability WHERE availability = 'review'")
-		courses = []
-		for row in rows:
-			courses = courses + list(row)
-		con.close()
-		return courses
-
-	def list_of_courses_status(self, status):
-		courses = controller.list_of_courses_status(status)
-		return courses
 
 	def get_MC_responses(self, course):
 		questions = controller.search_surveyID(course)
@@ -151,19 +49,7 @@ class Admin:
 			results[question_text] = sa_answer
 		return results
 
-admin = Admin()
-print(admin.get_SA_responses('SENG2011 17s2'))
-class Survey(Admin):
-	def __init__(self, course_filename):
-		Admin.__init__(self)
-		self.course_filename = course_filename
-
-	def search_for_course_questions(self, course_name):
-		questions = []
-		questions = controller.search_surveyID(course_name)
-		return questions
-
-	def get_courselist(self):
+	def get_course_list(self):
 		course_list = []
 		f = open(self.course_filename)
 		reader = csv.reader(f)
@@ -171,47 +57,136 @@ class Survey(Admin):
 			course_list = course_list + [" ".join(row)]
 		return course_list
 
-	def get_survey_status(self, course_name):
+	def get_status_of_course(self, course_name):
 		result = controller.get_survey_status(course_name)
 		return result
 
-	def change_survey_status(self, course_name, status):
-		controller.change_survey_status(course_name, status)
+class Student():
+	def __init__ (self, ID):
+		self.ID = ID
 
-	def add_question_to_survey(self, course_name, question_to_add):
+	def submit_answers(self, course_name, question, answer_picked):
+		questionID = controller.search_questionID(question)
+		controller.add_to_answer_database(course_name, questionID, answer_picked)
+
+	def get_courses(self):
+		con = sqlite3.connect("survey_database.db")
+		cur = con.cursor()
+		rows = cur.execute("SELECT course_name FROM student_enrolments WHERE id = ?", (self.ID,))
+		student_courses = []
+		for row in rows:
+			student_courses = student_courses + list(row)
+		con.close()
+		return student_courses
+
+	def get_semester(self):
+		con = sqlite3.connect("survey_database.db")
+		cur = con.cursor()
+		cur.execute("SELECT semester FROM student_enrolments WHERE id = ?", (self.ID,))
+		semester = cur.fetchone()
+		con.close()
+		return semester[0]
+
+	def survey_completed(self, course_name):
+		con = sqlite3.connect("survey_database.db")
+		cur = con.cursor()
+		cur.execute("UPDATE student_enrolments SET survey_completed = 'yes' WHERE id = ? AND course_name = ?", (self.ID, course_name))
+		con.commit()
+		con.close()
+
+	def check_if_survey_completed(self, course_name):
+		con = sqlite3.connect("survey_database.db")
+		cur = con.cursor()
+
+		cur.execute("SELECT survey_completed FROM student_enrolments WHERE id = ? AND course_name = ?", (self.ID, course_name))
+		result = cur.fetchone()
+		con.close()
+		return result[0]
+
+	def view_answers(self, question_to_see):
+		question_id = controller.search_questionID(question_to_see)
+		answer_list = controller.search_answerText(question_id)
+		return answer_list
+
+class Staff():
+	def __init__(self, ID):
+		self.ID = ID
+
+	def add_question_to_course(self, question, course_name):
 		#First find id of the question_to_add
-		question_id = controller.search_questionID(question_to_add)
+		question_id = controller.search_questionID(question)
 		controller.add_to_survey(course_name, question_id)
 
-	def get_questions_in_course(self, course_name):
-		all_questions = controller.search_surveyID(course_name)
-		return all_questions
-
-	def get_answers_to_questions(self, question_list):
-		question_answer = {}
-		for question in question_list:
-			question_answer[question] = super(Survey, self).view_answers(question)
-		return question_answer
-
-	def delete_question_from_survey(self, question_to_delete, course_name):
-		question_id = controller.search_questionID(question_to_delete)
+	def delete_question_from_course(self, question, course_name):
+		question_id = controller.search_questionID(question)
 		controller.delete_question_from_survey(question_id, course_name)
 
-	# Checks if the question we are trying to add to the survey is already added in
-	def check_for_duplicate(self, selected_course, selected_question):
+	def check_for_duplicates(self, selected_course, selected_question):
 		result = controller.check_for_duplicate(selected_course, selected_question)
 		if result: # If question already exists
 			return 1
 		else:
 			return 0
 
-class StudentAnswers:
-	def init(self):
-		pass
+	# Gets the pool of questions
+	def view_questions(self):
+		questions = controller.view_all_questions()
+		return questions
 
-	def add_answers(self, course_name, question, answer_picked):
-		questionID = controller.search_questionID(question)
-		controller.add_to_answer_database(course_name, questionID, answer_picked)
+	def view_answers(self, question_to_see):
+		question_id = controller.search_questionID(question_to_see)
+		answer_list = controller.search_answerText(question_id)
+		return answer_list
 
-survey = Survey('courses.csv')
-print(survey.check_for_duplicate('SENG2011 17s2', 'What is your favourite drink?'))
+	def get_course_status(self, course_name):
+		result = controller.get_survey_status(course_name)
+		return result
+
+	def list_of_courses_status(self, status):
+		courses = controller.list_of_courses_status(status)
+		return courses
+
+class Admin(Staff):
+	def __init__(self, ID):
+		Staff.__init__(self, ID)
+
+	def add_question(self, question, questionType, answers):
+		questionID = random.randint(1,100)
+		answerID = questionID
+		controller.add_questionText(questionID, question, questionType)
+		for answer in answers:
+			controller.add_answerText(answerID, questionID, answer)
+
+	def delete_question(self, question):
+		question_id = controller.search_questionID(question)
+		controller.delete_question(question_id)
+
+	def change_survey_status(self, course_name, status):
+		controller.change_survey_status(course_name, status)
+
+	def get_survey_availabilities(self):
+		survey_open = controller.count_survey_status('open')
+		survey_close = controller.count_survey_status('close')
+		survey_null = controller.count_survey_status('null')
+		availability = [survey_open, survey_close, survey_null]
+		return availability
+
+class Teacher(Staff):
+	def __init__(self, ID):
+		Staff.__init__(self, ID)
+
+	def pass_review(self, course_name):
+		controller.change_survey_status(course_name, 'open')
+
+	def get_surveys_to_review(self):
+		con = sqlite3.connect("survey_database.db")
+		cur = con.cursor()
+		rows = cur.execute("SELECT course_name FROM survey_availability WHERE availability = 'review'")
+		courses = []
+		for row in rows:
+			courses = courses + list(row)
+		con.close()
+		return courses
+
+admin = Teacher('50')
+print(admin.view_questions())
--- project/routes.py
+++ project/routes.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, url_for, redirect, current_app
-from classes import Admin, Survey, StudentAnswers, StudentDatabase
+from classes import Survey_system, Student, Staff, Admin, Teacher
 from flask_login import LoginManager, login_required, login_user, UserMixin, logout_user, current_user
 from flask_sqlalchemy import SQLAlchemy
 from functools import wraps
@@ -10,7 +10,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///survey_database.db'
 
 db = SQLAlchemy(app)
 
-admin = Admin()
 login_manager = LoginManager()
 login_manager.init_app(app)
 
@@ -52,9 +51,10 @@ def index():
 	if current_user.is_authenticated:
 		if current_user.type == 'student':
 			return redirect(url_for('student_dashboard'))
-		elif current_user.type == 'staff' :
+		elif current_user.type == 'admin' :
 			return redirect(url_for('dashboard'))
-
+		elif current_user.type == 'staff':
+			return redirect(url_for('staff_dashboard'))
 	# If not logged in
 	if request.method == "POST":
 		username = request.form['ID']
@@ -67,6 +67,8 @@ def index():
 			login_user(registered_user)
 			# Then redirect to certain webpage depending on role
 			if registered_user.type == 'student':
+				student = Student(registered_user.get_id())
+				survey_system = Survey_system('courses.csv', student)
 				return redirect(url_for('student_dashboard'))
 			elif registered_user.type == 'staff' :
 				return redirect(url_for('staff_dashboard'))
@@ -85,20 +87,35 @@ def logout():
 def dashboard():
 	# We then need to check the type of the user, if its a staff we direct to the
 	# staff dashboard, else if its student they go to student dashboard
+	admin = Admin(current_user.get_id())
+	survey_system = Survey_system('courses.csv', admin)
 	results = admin.get_survey_availabilities()
 	return render_template('dashboard.html', open = results[0], close = results[1], null = results[2],
 courses_open = admin.list_of_courses_status('open'))
 
 @app.route("/responses", methods = ["POST", "GET"])
 def responses():
-	course_list = admin.list_of_courses_status('close')
+	if current_user.get_type() == 'student':
+		student = Student(current_user.get_id())
+		survey_system = Survey_system('courses.csv', student)
+
+		courses = student.get_courses()
+		course_list = []
+		# Then get the courses that are closed
+		for course in courses:
+			if survey_system.get_status_of_course(course + ' ' +student.get_semester()) == 'close':
+				course_list.append(course + ' ' + student.get_semester())
+	else:
+		admin = Admin(current_user.get_id())
+		survey_system = Survey_system('courses.csv', admin)
+		course_list = admin.list_of_courses_status('close')
 
 	if request.method == "POST":
 		# Get the questions associated with the survey
 		# Then determine if the question is MC or short answer
 		# If MC then we get the answers and make a chart
 		course_name = request.form.get('course_selected')
-		MC_responses = admin.get_MC_responses(course_name)
+		MC_responses = survey_system.get_MC_responses(course_name)
 
 		return render_template('responses.html', course_list = course_list, MC_responses = MC_responses,
 	course_name = course_name)
@@ -107,7 +124,9 @@ def responses():
 
 @app.route("/sa_responses_<course_name>", methods = ["POST", "GET"])
 def sa_responses(course_name):
-	sa_results = admin.get_SA_responses(course_name)
+	admin = Admin(current_user.get_id())
+	survey_system = Survey_system('courses.csv', admin)
+	sa_results = survey_system.get_SA_responses(course_name)
 
 	if request.method == "POST":
 		question_text = request.form.get('q_selected')
@@ -117,7 +136,9 @@ def sa_responses(course_name):
 @app.route("/staff_dashboard", methods = ["POST", "GET"])
 @login_required(role = "staff")
 def staff_dashboard():
-	courses = admin.surveys_to_review()
+	teacher = Teacher(current_user.get_id())
+	survey_system = Survey_system('courses.csv', teacher)
+	courses = teacher.get_surveys_to_review()
 
 	if request.method == "POST":
 		course_name = request.form.get('course_name')
@@ -127,10 +148,11 @@ def staff_dashboard():
 @app.route("/student_dashboard", methods = ["POST", "GET"])
 @login_required(role = "student")
 def student_dashboard():
-	database = StudentDatabase()
+	student = Student(current_user.get_id())
+	survey_system = Survey_system('courses.csv', student)
 	#database.add_info_enrollments()
-	courses = database.get_student_courses(current_user.get_id())
-	semester = database.get_student_semester(current_user.get_id())
+	courses = student.get_courses()
+	semester = student.get_semester()
 
 	if request.method == "POST":
 		course_name = request.form.get('course_name')
@@ -141,48 +163,51 @@ def student_dashboard():
 @app.route("/review_<course_name>", methods = ["POST", "GET"])
 @login_required(role = 'staff')
 def review_course(course_name):
-	survey = Survey('courses.csv')
+	teacher = Teacher(current_user.get_id())
+	survey_system = Survey_system('courses.csv', teacher)
 	question_answer = {}
-	question_in_course = survey.get_questions_in_course(course_name)
+	question_in_course = survey_system.get_question(course_name)
 
 	for question in question_in_course:
-		question_answer[question] = admin.view_answers(question)
+		question_answer[question] = teacher.view_answers(question)
 
 	if request.method == "POST":
 		if (request.form.getlist('question-selected')):
 			selected_question = request.form.get('question-selected')
-			survey.add_question_to_survey(course_name, selected_question)
-			status = survey.get_survey_status(course_name)
+			teacher.add_question_to_course(selected_question, course_name)
+			status = teacher.get_course_status(course_name)
 			# Update the question list
-			question_in_course = survey.get_questions_in_course(course_name)
+			question_in_course = survey_system.get_question(course_name)
 			for question in question_in_course:
-				question_answer[question] = admin.view_answers(question)
+				question_answer[question] = teacher.view_answers(question)
 
 		elif request.form['submit'] == 'open':
 			selected_course = request.form.get('course_selected')
-			survey.change_survey_status(course_name, '')
-			status = survey.get_survey_status(course_name)
+			teacher.pass_review(selected_course)
+			status = teacher.get_course_status(selected_course)
 			return redirect(url_for('staff_dashboard'))
 
 		elif request.form['submit'] == 'delete_question':
 			selected_course = request.form.get('course_selected')
 			question_to_delete = request.form.getlist('checkbox_value')
 			for question in question_to_delete:
-				survey.delete_question_from_survey(question, course_name)
+				teacher.delete_question_from_course(question, course_name)
+
 			# Refresh the question_answer list
 			question_answer = {}
-			question_in_course = survey.get_questions_in_course(course_name)
+			question_in_course = survey_system.get_question(course_name)
 			for question in question_in_course:
-				question_answer[question] = admin.view_answers(question)
-		return render_template('review_survey.html', course_name = course_name, question_answer = question_answer,
-	question_list = admin.open_questionfile(), status = survey.get_survey_status(course_name))
+				question_answer[question] = teacher.view_answers(question)
 
 	return render_template('review_survey.html', course_name = course_name, question_answer = question_answer,
-question_list = admin.open_questionfile(), status = survey.get_survey_status(course_name))
+question_list = teacher.view_questions(), status = teacher.get_course_status(course_name))
 
 @app.route("/question", methods = ["POST", "GET"])
 @login_required(role = "admin")
 def question_page():
+	admin = Admin(current_user.get_id())
+	survey_system = Survey_system('courses.csv', admin)
+
 	question_from_server = []
 	answer_list = []
 	error = 'negative'
@@ -194,14 +219,14 @@ def question_page():
 			if len(answer_list) == 1 and question_type == 'MC':
 				error = 'positive'
 			else:
-				admin.add_question(question_from_server, answer_list, question_type)
+				admin.add_question(question_from_server, question_type, answer_list)
 		elif request.form['submit'] == 'delete_question':
 			question_to_delete = request.form.getlist('checkbox_value')
 			for question in question_to_delete:
 				admin.delete_question(question)
 			# Returns a list of the options selected
 	question_answer = {}
-	question_list = admin.open_questionfile()
+	question_list = admin.view_questions()
 	for question in question_list:
 		question_answer[question] = admin.view_answers(question)
 	return render_template('question_page.html', question_answer = question_answer, error = error)
@@ -210,36 +235,37 @@ selected_course = ''
 @app.route("/survey_creation", methods = ["POST", "GET"])
 @login_required(role = "admin")
 def survey_creation():
-	survey = Survey('courses.csv')
+	admin = Admin(current_user.get_id())
+	survey_system = Survey_system('courses.csv', admin)
 
 	if request.method == "POST":
 	#if (request.form.get('course-selected')):
 		question_answer = {}
 		if request.form.get('course-selected'):
 			selected_course = request.form.get('course-selected')
-			question_in_course = survey.get_questions_in_course(selected_course)
+			question_in_course = survey_system.get_question(selected_course)
 
 			for question in question_in_course:
 				question_answer[question] = admin.view_answers(question)
 
-			status = survey.get_survey_status(selected_course)
+			status = survey_system.get_status_of_course(selected_course)
 
 		elif request.form['submit'] == 'open':
 			selected_course = request.form.get('course_selected')
-			status = survey.get_survey_status(selected_course)
-			survey.change_survey_status(selected_course, status)
-			status = survey.get_survey_status(selected_course)
+			status = survey_system.get_status_of_course(selected_course)
+			admin.change_survey_status(selected_course, status)
+			status = survey_system.get_status_of_course(selected_course)
 
 		elif (request.form.getlist('question-selected')):
 			selected_course = request.form.get('course_selected')
 			selected_question = request.form.get('question-selected')
 			# Check the question has not already been added:
-			check = survey.check_for_duplicate(selected_course, selected_question)
+			check = admin.check_for_duplicates(selected_course, selected_question)
 			if (check == 0):	# Question not yet added
-				survey.add_question_to_survey(selected_course, selected_question)
-			status = survey.get_survey_status(selected_course)
+				admin.add_question_to_course(selected_question, selected_course)
+			status = survey_system.get_status_of_course(selected_course)
 			# Update the question list
-			question_in_course = survey.get_questions_in_course(selected_course)
+			question_in_course = survey_system.get_question(selected_course)
 			for question in question_in_course:
 				question_answer[question] = admin.view_answers(question)
 
@@ -247,53 +273,61 @@ def survey_creation():
 			selected_course = request.form.get('course_selected')
 			question_to_delete = request.form.getlist('checkbox_value')
 			for question in question_to_delete:
-				survey.delete_question_from_survey(question, selected_course)
+				admin.delete_question_from_course(question, selected_course)
+			# Update the question_answer dictionary
+			question_answer = {}
+			question_in_course = survey_system.get_question(selected_course)
+
+			for question in question_in_course:
+				question_answer[question] = admin.view_answers(question)
+
+			status = survey_system.get_status_of_course(selected_course)
 
 		elif request.form['submit'] == 'put_into_review':
 			selected_course = request.form.get('course_selected')
-			survey.change_survey_status(selected_course, 'review')
-			status = survey.get_survey_status(selected_course)
+			admin.change_survey_status(selected_course, 'review')
+			status = survey_system.get_status_of_course(selected_course)
 
-		return render_template("survey_creation.html", course_list = survey.get_courselist(),
-			question_list = admin.open_questionfile(), question_answer = question_answer,
+		return render_template("survey_creation.html", course_list = survey_system.get_course_list(),
+			question_list = admin.view_questions(), question_answer = question_answer,
 			course_selected = selected_course, status = status)
 
-
-	return render_template("survey_creation.html", course_list = survey.get_courselist(),
-		question_list = admin.open_questionfile())
+	return render_template("survey_creation.html", course_list = survey_system.get_course_list(),
+		question_list = admin.view_questions())
 
 @app.route("/answer_survey/<course_name> <semester>", methods = ["POST", "GET"])
 @login_required(role = "student")
 def answer_survey(course_name, semester):
-	# Check that the student is part of the course
-	student_database = StudentDatabase()
+	student = Student(current_user.get_id())
+	survey_system = Survey_system('courses.csv', student)
 	question_answer = {}
 
 	# Check that the student is part of the course before they can answer survey
-	if course_name in student_database.get_student_courses(current_user.username) and semester in student_database.get_student_semester(current_user.username):
+	if course_name in student.get_courses() and semester in student.get_semester():
 		# Check that surveys for that course exist, if it doesn't return an error message
-		survey = Survey('courses.csv')
-		student_answers = StudentAnswers()
-		questions = survey.search_for_course_questions(course_name + ' ' + semester)
+		questions = survey_system.get_question(course_name + ' ' + semester)
 		if not questions:
 			status = 'has no surveys'
 		else:
 			# If there are questions we check that the student has not answered the survey before:
-			if student_database.check_if_survey_completed(current_user.username, course_name) == 'yes':
+			if student.check_if_survey_completed(course_name) == 'yes':
 				status = 'survey already completed'
 			else:
 			# If there are questions, we want to check that the survey is open for answers
-				if survey.get_survey_status(course_name + ' ' + semester) == 'open':
+				if survey_system.get_status_of_course(course_name + ' ' + semester) == 'open':
 					# we want to find the answers then make a dictionary
 					# so that the answer and question are related
 					status = 'open'
-					question_answer = survey.get_answers_to_questions(questions)
+
+					for question in questions:
+						question_answer[question] = student.view_answers(question)
+
 					if request.method == "POST":
 						for k in question_answer:
 							answer = request.form.get(k)
-							student_answers.add_answers(course_name + ' ' + semester, k, answer)
+							student.submit_answers(course_name + ' ' + semester, k, answer)
 						# Then we update the database to indicate the student has completed the survey
-						student_database.completion_of_survey(current_user.username, course_name)
+						student.survey_completed(course_name)
 						return redirect(url_for('student_dashboard'))
 				else:
 					status = 'survey is closed'
